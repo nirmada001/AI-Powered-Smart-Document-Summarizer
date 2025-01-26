56c68cac78c11ccd935fb2b831adac761ac748dc
Nirmada <nirmada0504@gmail.com>
2025-01-26 19:43:18 +0530

    Login Register UI Enhancement

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,47 +1,75 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import '../css/login.css'
 
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [success, setSuccess] = useState("");
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const handleLogin = async (e)=>{
-        e.preventDefault();
-        setError("");
-        setSuccess("");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+    setLoading(true);
 
-        try{
-            const response = await axios.post("http://127.0.0.1:5000/api/users/login",{
-                email,
-                password
-            });
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/api/users/login", {
+        email,
+        password
+      });
 
-            setSuccess("Login Successful");
-            localStorage.setItem("token", response.data.access_token); // Save Token
-            setTimeout(()=>navigate("/"), 1000); //Redirect to home page after 1 sec
+      setSuccess("Login Successful");
+      localStorage.setItem("token", response.data.access_token); // Save Token
+      setTimeout(() => navigate("/"), 1000); //Redirect to home page after 1 sec
 
-        }catch(err){
-            setError(err.response?.data?.error || "Login Failed. Try agin.");
-        }
-    };
+    } catch (err) {
+      setError(err.response?.data?.error || "Login Failed. Try agin.");
+    }
+  };
   return (
-    <div>
-      <h2>Login</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {success && <p style={{ color: "green" }}>{success}</p>}
-      <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <button type="submit">Login</button>
-      </form>
-      <p>Don't have an account? <a href="/register">Register</a></p>
+    <div className='main-div'>
+      <div className='login-div'>
+        <h1 className='title'>Login</h1>
+        {error && <p className='error-message'>{error}</p>}
+        {success && <p className='success-message'>{success}</p>}
+        <form onSubmit={handleLogin}>
+          <div className='form-group'>
+            <label htmlFor="email" className='label'>Email</label><br />
+            <input type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className='input'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="passowrd" className='label'>Password</label><br />
+            <input type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className='input'
+            />
+          </div>
+          <p>Don't have an account? <Link to="/register" className='Link'>Register</Link></p>
+          <button type="submit" disabled={loading} className='login-button'>
+            {loading ? 'Loging in...' : 'Login'}
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
 
 export default Login
+
+
+
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,48 +1,79 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import '../css/register.css'
 
 
 const Register = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [success, setSuccess] = useState("");
-    const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const navigate = useNavigate();
 
-    const handleRegister = async (e)=>{
-        e.preventDefault();
-        setError("");
-        setSuccess("");
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+    setLoading(true);
 
-        try{
-            await axios.post("http://127.0.0.1:5000/api/users/register",{
-                name,
-                email,
-                password
-            });
+    try {
+      await axios.post("http://127.0.0.1:5000/api/users/register", {
+        name,
+        email,
+        password
+      });
 
-            setSuccess("Registration Successful");
-            setTimeout(()=>navigate("/login"), 1000); //Redirect to login page after 1 sec
+      setSuccess("Registration Successful");
+      setTimeout(() => navigate("/login"), 1000); //Redirect to login page after 1 sec
 
-        }catch(err){
-            setError(err.response?.data?.error || "Registration Failed. Try agin.");
-        }
-    
-    };
+    } catch (err) {
+      setError(err.response?.data?.error || "Registration Failed. Try agin.");
+    }
+
+  };
   return (
-    <div>
-      <h2>Register</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {success && <p style={{ color: "green" }}>{success}</p>}
-      <form onSubmit={handleRegister}>
-        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <button type="submit">Register</button>
-      </form>
-      <p>Already have an account? <a href="/login">Login</a></p>
+    <div className='main-div'>
+      <div className='register-div'>
+        <h1 className='title'>Register</h1>
+        {error && <p className='error-message'>{error}</p>}
+        {success && <p className='success-message'>{success}</p>}
+        <form onSubmit={handleRegister}>
+          <div className='form-group'>
+            <label htmlFor="name" className='label'>Name</label><br />
+            <input type="text"
+              placeholder="Name"
+              value={name} onChange={(e) => setName(e.target.value)}
+              required
+              className='input'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="email" className='label'>Email</label><br />
+            <input type="email"
+              placeholder="Email"
+              value={email} onChange={(e) => setEmail(e.target.value)}
+              required
+              className='input'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="password" className='label'>Password</label><br />
+            <input type="password"
+              placeholder="Password"
+              value={password} onChange={(e) => setPassword(e.target.value)}
+              required
+              className='input'
+            />
+          </div>
+          <p>Already have an account? <Link to="/login" className='Link'>Login</Link></p>
+          <button type="submit" disabled={loading} className='register-button'>
+            {loading ? 'Registering...' : 'Register'}
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
